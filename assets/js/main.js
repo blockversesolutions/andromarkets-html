268b6a876d5d3f76e43d68073efa04a44262f02c
juel <juelcse2019@gmail.com>
2024-11-20 00:13:33 +0600

    new page add

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,59 @@ jQuery(document).ready(function ($) {
   // });
   
 });
+(function () {
+  const quantityContainers = document.querySelectorAll(".quantity");
+
+  quantityContainers.forEach((quantityContainer) => {
+    const minusBtn = quantityContainer.querySelector(".minus");
+    const plusBtn = quantityContainer.querySelector(".plus");
+    const inputBox = quantityContainer.querySelector(".input-box");
+
+    updateButtonStates();
+
+    quantityContainer.addEventListener("click", handleButtonClick);
+    inputBox.addEventListener("input", handleQuantityChange);
+
+    function updateButtonStates() {
+      const value = parseInt(inputBox.value);
+      minusBtn.disabled = value <= 1;
+      // Remove the condition to disable the plus button based on max value
+    }
+
+    function handleButtonClick(event) {
+      if (event.target.classList.contains("minus")) {
+        decreaseValue();
+      } else if (event.target.classList.contains("plus")) {
+        increaseValue();
+      }
+    }
+
+    function decreaseValue() {
+      let value = parseInt(inputBox.value);
+      value = isNaN(value) ? 1 : Math.max(value - 1, 1);
+      inputBox.value = value;
+      updateButtonStates();
+      handleQuantityChange();
+    }
+
+    function increaseValue() {
+      let value = parseInt(inputBox.value);
+      value = isNaN(value) ? 1 : value + 1; // No max limit now
+      inputBox.value = value;
+      updateButtonStates();
+      handleQuantityChange();
+    }
+
+    function handleQuantityChange() {
+      let value = parseInt(inputBox.value);
+      value = isNaN(value) ? 1 : value;
+
+      // Execute your code here based on the updated quantity value
+      console.log("Quantity changed:", value);
+    }
+  });
+})();
+
 // scroll bar js
 document.addEventListener('DOMContentLoaded', function () {
   new SimpleBar(document.getElementById('scrollbar'));
